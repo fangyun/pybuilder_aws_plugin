ae9deaa8134fa26209519e7f8d23915538cb0ca4
Yun Fang <yun.fang@thomsonreuters.com>
2018-06-25 14:53:53 +0800

    rename module name

--- build.py
+++ build.py
@@ -5,10 +5,7 @@ from pybuilder.core import Author, init, use_plugin
 from pybuilder.vcs import VCSRevision
 
 use_plugin('python.core')
-use_plugin('python.unittest')
 use_plugin('python.install_dependencies')
-use_plugin('python.flake8')
-use_plugin('python.coverage')
 use_plugin('python.distutils')
 
 
